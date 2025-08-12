2081b0dbf7c8d3b5aba9aabb4f1dd48ed11c2e08
Fabian Sabogal <me@fvioz.dev>
2025-08-12 17:47:55 +0200

    Add db app schema change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_12_141453) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_12_144248) do
+  create_table "apps", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_apps_on_name", unique: true
+  end
+
   create_table "contents", force: :cascade do |t|
     t.string "type"
     t.string "original_title"
