0b81e529c447c793c34597459d1aa8319fe995e8
Fabian Sabogal <me@fvioz.dev>
2025-08-12 17:48:23 +0200

    Include all component queries into the root query file

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -20,12 +20,9 @@ module Types
 
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
-
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
-    end
+    include Channels::QueryType
+    include Contents::QueryType
+    include Movies::QueryType
+    include TvShows::QueryType
   end
 end
