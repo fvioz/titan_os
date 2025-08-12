4de1682a80c8828d06b54e534747bdd6315d674d
Fabian Sabogal <me@fvioz.dev>
2025-08-12 16:42:13 +0200

    Seed data for content model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,28 @@
-User.create!(
-  username: "test",
+Users::User.create!(
+  name: "test",
   email: "test@test.com",
   password: 'password'
 )
+
+JSON.parse(File.read(Rails.root.join("db", "seeds", "streams_data.json"))).each do |type, contents|
+  puts "Seeding #{type}..."
+
+  contents.each do |content|
+    model = case content["type"]
+            when "movie"
+              Movies::Movie
+            when "tv_show"
+              TvShows::TvShow
+            when "channel"
+              Channels::Channel
+            else
+              next
+            end
+
+    model.create!(
+      original_title: content["original_title"],
+      year: content["year"],
+      duration_in_seconds: content["duration_in_seconds"],
+    )
+  end
+end
